40f5ac78670c54f37a3efdeb07b793105f8c7fc5
Bảo Trung <trung.tnb.60cntt@ntu.edu.vn>
2022-04-04 21:33:02 +0700

    Complete BaiTHIntent

--- BaiTHIntent/app/src/main/java/cntt61/mssv60137367/baithintent/HomeActivity.java
+++ BaiTHIntent/app/src/main/java/cntt61/mssv60137367/baithintent/HomeActivity.java
@@ -2,7 +2,10 @@ package cntt61.mssv60137367.baithintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+    public void Nhap_QuayVe(View v){
+        EditText edHT = (EditText)findViewById(R.id.editKQ);
+        // Lấy dữ Liệu
+        String hoten = edHT.getText().toString();
+
+        // Tạo mới Intent
+        Intent iKQnhaplieu = new Intent();
+        iKQnhaplieu.putExtra("HT",hoten);
+        // Gửi kết quả về cho Activity đã gọi nó
+        setResult(RESULT_OK, iKQnhaplieu);
+        // Đóng Activity nhập Liệu (này)
+        finish();
     }
 }
--- BaiTHIntent/app/src/main/java/cntt61/mssv60137367/baithintent/LoginActivity.java
+++ BaiTHIntent/app/src/main/java/cntt61/mssv60137367/baithintent/LoginActivity.java
@@ -7,11 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     EditText edtusername, edtpass, edtmail;
-    Button btnok;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,24 +21,21 @@ public class LoginActivity extends AppCompatActivity {
         edtusername = (EditText) findViewById(R.id.edtUsername);
         edtpass = (EditText) findViewById(R.id.edtPass);
         edtmail = (EditText) findViewById(R.id.edtMail);
-        btnok = (Button) findViewById(R.id.btnOK);
-
-        btnok.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String username = "baotrung";
-                String password = "123";
-                String mall = "trung@gmail.com";
-                if (edtusername.getText().toString().equals(username) && edtpass.getText().toString().equals(password) && edtmail.getText().toString().equals(mall)){
-                    Toast.makeText(getApplicationContext(),"Đăng Nhập Thành Công",Toast.LENGTH_LONG).show();
-                    Intent iManhinhHome = new Intent(LoginActivity.this, HomeActivity.class);
-                    startActivity(iManhinhHome);
-                }
-                else{
-                    Toast.makeText(getApplicationContext(),"Tài Khoản Hoặc Mật Khẩu Sai",Toast.LENGTH_LONG).show();
-                }
-            }
-
-        });
+    }
+    public void XulyLogin(View v) {
+        String username = "baotrung";
+        String password = "123";
+        String mail = "trung@gmail.com";
+        if (edtusername.getText().toString().equals(username) && edtpass.getText().toString().equals(password) && edtmail.getText().toString().equals(mail)){
+            Toast.makeText(getApplicationContext(),"Đăng Nhập Thành Công",Toast.LENGTH_SHORT).show();
+            /*String hoten = data.getStringExtra("HT");
+            TextView tvHT = (TextView)findViewById(R.id.editKQ);
+            tvHT.setText(hoten);*/
+            Intent iManhinhHome = new Intent(LoginActivity.this, HomeActivity.class);
+            startActivity(iManhinhHome);
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Tài Khoản Hoặc Mật Khẩu Sai",Toast.LENGTH_LONG).show();
+        }
     }
 }
