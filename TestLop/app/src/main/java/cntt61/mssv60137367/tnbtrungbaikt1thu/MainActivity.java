02fb7ac3e87d5184f25f576cbeb7cdefabbc95ed
Bảo Trung <trung.tnb.60cntt@ntu.edu.vn>
2022-04-05 14:04:57 +0700

    Update BaiKT1

--- TestLop/app/src/main/java/cntt61/mssv60137367/tnbtrungbaikt1thu/MainActivity.java
+++ TestLop/app/src/main/java/cntt61/mssv60137367/tnbtrungbaikt1thu/MainActivity.java
@@ -2,13 +2,31 @@ package cntt61.mssv60137367.tnbtrungbaikt1thu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtusername, edtpass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //ánh xạ các biến
+        edtusername = (EditText) findViewById(R.id.edtUser);
+        edtpass = (EditText) findViewById(R.id.edtPass);
+    }
+    public void XulyLogin(View v) {
+        String username = "baotrung";
+        String password = "123";
+        if (edtusername.getText().toString().equals(username) && edtpass.getText().toString().equals(password)){
+            Toast.makeText(getApplicationContext(),"User and Password is correct",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"User and Password is wrong”",Toast.LENGTH_LONG).show();
+        }
     }
 }
