071c23f16e71f1c7c9626ad107a905233e5b4c28
Bảo Trung <trung.tnb.60cntt@ntu.edu.vn>
2022-04-11 15:31:18 +0700

    complete BaiKT2

--- TNBTrungBKT2thu/app/src/main/java/cntt61/mssv60137367/tnbtrungbkt2thu/MainActivity.java
+++ TNBTrungBKT2thu/app/src/main/java/cntt61/mssv60137367/tnbtrungbkt2thu/MainActivity.java
@@ -3,12 +3,47 @@ package cntt61.mssv60137367.tnbtrungbkt2thu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btntinh;
+    EditText edtNum;
+    RadioButton rd13, rd15, rd18;
+    TextView ketqua;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btntinh = (Button)findViewById(R.id.button);
+        edtNum = (EditText) findViewById(R.id.number);
+        rd13 = (RadioButton) findViewById(R.id.radio13);
+        rd15 = (RadioButton) findViewById(R.id.radio15);
+        rd18 = (RadioButton) findViewById(R.id.radio18);
+        ketqua = (TextView) findViewById(R.id.result);
+    }
+    public void TinhToan(View view){
+        Float num = edtNum.getAlpha();
+        if(rd13.isChecked()){
+            num = num*13/100;
+            rd15.setChecked(false);
+            rd18.setChecked(false);
+        }
+        if(rd15.isChecked()){
+            num = num*15/100;
+            rd13.setChecked(false);
+            rd18.setChecked(false);
+        }
+        if(rd18.isChecked()){
+            num = num*18/100;
+            rd13.setChecked(false);
+            rd15.setChecked(false);
+        }
+        String s =String.valueOf(num);
+        ketqua.setText(s);
     }
 }
